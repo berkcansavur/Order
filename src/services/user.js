1d6c2a66bb733bf51a6c3d295242575511bfe0f4
Berkcan <berkcansavur@gmail.com>
2023-04-28 03:31:03 +0300

    -user methods such as createuser, deleteuser, updateuser etc. are created on user service and used in user router.

--- src/services/user.js
+++ src/services/user.js
@@ -13,10 +13,56 @@ async function createUser(req) {
         throw new Error(error)
     }
 }
-async function findUser(id){
-    return await User.findById(id);
+async function findUser(user){
+    const returnedUser = await User.findById(user._id);
+    return returnedUser;
 }
+async function loginUser(email,password){
+    try {
+        const user = await User.findByCredentials(email, password);
+        const token = await user.generateAuthToken();
+        const loggedUser ={
+            user,
+            token
+        }
+        return loggedUser;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+async function logoutUser(reqUser,reqToken){
+    try {
+        reqUser.tokens = reqUser.tokens.filter((token)=>{
+            return token.token !== reqToken;
+        })
+        await reqUser.save();
+        return ('user '+reqUser.email+' has been logged out');
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+async function deleteUser(userId){
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        return ('Removed User '+user.email+' successfully.');
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+async function updateUser(userId,updates){
+    try {
+        const user = await User.findByIdAndUpdate(userId,updates,{new:true});
+        return ('Updated User '+user.email+' successfully.');
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
 module.exports = {
     createUser,
-    findUser
+    deleteUser,
+    findUser,
+    updateUser,
+    loginUser,
+    logoutUser
 };
